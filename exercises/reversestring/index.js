146fd85b1cce40820143d028ec087faec3328945
ShobhitRatan <shobhit.ratan@baruchmail.cuny.edu>
2021-04-28 15:11:42 +0530

    Added Reverse String solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // My Solution
+  // return str.split('').reduce((rev, char) => char + rev, ''); 
+  // Solution #1 
+  // const arr = str.split('');
+  // arr.reverse();
+  // return arr.join(''); 
+  // return str.split('').reverse().join(''); 
+  // Solution #2 
+  // let reversed = '';
+  // for (let character of str) {
+  //   reversed = character + reversed;
+  // }
+  // return reversed; 
+  // Solution #3 
+ 
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
+reverse('asdf'); 
 
 module.exports = reverse;
